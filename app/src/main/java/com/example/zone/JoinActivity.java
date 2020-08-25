83e6dd6f5a82df9ccd61202d650eb95af6f95219
hhh131 <gmlrnjs131@naver.com>
2020-08-25 22:33:18 +0900

    FireBase변경

--- app/src/main/java/com/example/zone/JoinActivity.java
+++ app/src/main/java/com/example/zone/JoinActivity.java
@@ -26,11 +26,13 @@ public class JoinActivity extends AppCompatActivity {
     EditText id,pwd,name;
     Button Signbtn;
     String data;
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
     //1.Firebase 실시간 DB 관리 객체 얻어오기.
     DatabaseReference myRef = database.getReference();
     //2.저장시킬 노드 참조 객체 가져오기
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,11 +45,15 @@ public class JoinActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         data=id.getText().toString();
 
+
+
+        myRef.child("User").push().setValue("asdf");
+
         Signbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myRef.setValue(data);
-
+                myRef.child("User").push().setValue("asdf");
+            Toast.makeText(getApplicationContext(),"asdasd",Toast.LENGTH_LONG).show();
                 //showMsg();
             }
         });
