aece2c23ebbc269cedbae93b4a92b716b124f53e
hhh131 <gmlrnjs131@naver.com>
2020-09-02 23:38:19 +0900

    List

---
+++ app/src/main/java/com/example/zone/List.java
@@ -0,0 +1,41 @@
+package com.example.zone;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+
+public class List extends AppCompatActivity {
+    ArrayList<String> midList;
+    ArrayAdapter<String> adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+       midList =
+                new ArrayList<String>();
+        ListView list = (ListView) findViewById(R.id.listView1);
+
+        adapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1, midList);
+        list.setAdapter(adapter);
+
+
+        midList.add("asd");
+        midList.add("as123d");
+
+        adapter.notifyDataSetChanged();
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/zone/Main.java
+++ app/src/main/java/com/example/zone/Main.java
@@ -29,7 +29,7 @@ public class Main extends AppCompatActivity {
     zone.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+             Intent intent = new Intent(getApplicationContext(),List.class);
              startActivity(intent);
         }
     });
